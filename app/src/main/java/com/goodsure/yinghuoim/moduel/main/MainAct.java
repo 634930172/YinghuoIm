7a140b0dc0fea07d1715004661725f53fbe423a2
lenovo <634930172@qq.com>
2018-03-09 09:58:04 +0800

    动态加

--- app/src/main/java/com/goodsure/yinghuoim/moduel/main/MainAct.java
+++ app/src/main/java/com/goodsure/yinghuoim/moduel/main/MainAct.java
@@ -47,7 +47,7 @@ public class MainAct extends BaseAct<MainView, MainPresenter> implements MainVie
     @Override
     public void DataCallBack(String str) {
         LogUtil.e("数据是>>>>>" + str);
-        //aslkjhlsfds 测试提交123
+        //aslkjhlsfds 测试提交321
     }
 
 
